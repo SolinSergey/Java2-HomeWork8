6de9d5a069d72f14810100c17283e0fcdca46dee
Sergey <livegrafics@rambler.ru>
2021-12-07 12:04:57 +0300

    Correction

--- src/main/java/NetworkChat.java
+++ src/main/java/NetworkChat.java
@@ -4,17 +4,20 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class NetworkChat extends JFrame {
     private JTextField message;
     private JTextArea chatArea;
     private JTextArea clientsList;
+    private JButton sendButton;
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
     private String nick = "";
+    private boolean timeOutStatus=false;
 
-    public NetworkChat(){
+    public NetworkChat() throws IOException {
         setTitle("Сетевой чат");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(300,300,400,400);
@@ -35,18 +38,31 @@ public class NetworkChat extends JFrame {
         });
         message.setText("/auth login1 pass1");
         downPanel.add(message);
-        JButton sendButton = new JButton("Отправить");
+        sendButton = new JButton("Отправить");
         sendButton.addActionListener(e -> {
             sendMessage();
         });
         downPanel.add(sendButton);
         add(downPanel,BorderLayout.PAGE_END);
         setVisible(true);
+        openConnection();
+        startTread();
+
     }
     private void closeConnection(){
-        chatArea.append("# Вы вышли из чата\n");
-        message.setText("/auth login1 pass1");
-        clientsList.setText("");
+        if (!timeOutStatus) {
+            chatArea.append("# Вы вышли из чата\n");
+            message.setText("/auth login1 pass1");
+            clientsList.setText("");
+        }
+        else{
+            chatArea.append("Истекло время отведенное на авторизацию.\n");
+            chatArea.append("Соединение разорвано\n");
+            chatArea.append("Перезапустите приложение");
+            message.setText("");
+            message.setEnabled(false);
+            sendButton.setEnabled(false);
+        }
         try {
             in.close();
             out.close();
@@ -55,45 +71,59 @@ public class NetworkChat extends JFrame {
             e.printStackTrace();
         }
     }
+    public void openConnection() throws IOException {
+        socket = new Socket("localhost",8189);
+        in = new DataInputStream((socket.getInputStream()));
+        out = new DataOutputStream((socket.getOutputStream()));
+        socket.setSoTimeout(120000);
+        timeOutStatus = false;
+    }
+
+    public void startTread(){
+        new Thread(()->{
+            try{
+                while (true && !timeOutStatus){
+                    String strFromServer = in.readUTF();
+                    if (strFromServer.startsWith("/authok")){
+                        nick = strFromServer.split(" ")[1];
+                        chatArea.append("# Вы авторизованы как: " +nick + "\n");
+                        socket.setSoTimeout(0);
+                        break;
+                    }
+                    chatArea.append(strFromServer + "\n");
+                }
+                while (true && !timeOutStatus){
+                    String strFromServer = in.readUTF();
+                    if (strFromServer.equalsIgnoreCase("/end")){
+                        break;
+                    }
+                    if (strFromServer.startsWith("/clients")){
+                        String clients = strFromServer.substring(9);
+                        clientsList.setText(clients.replace(' ','\n'));
+                    }
+                    else {
+                        chatArea.append(strFromServer + "\n");
+                    }
+                }
+            } catch (SocketTimeoutException e){
+                System.out.println("Time out connection");
+                timeOutStatus = true;
+            } catch (Exception e){
+                e.printStackTrace();
+            } finally {
+             closeConnection();
+            }
+        }).start();
+    }
+
     public void sendMessage(){
         String trimmedMessage = this.message.getText().trim();
         if (!trimmedMessage.isEmpty()){
             message.setText("");
             try{
-                if (socket == null || socket.isClosed()){
-                    socket = new Socket("localhost",8189);
-                    in = new DataInputStream((socket.getInputStream()));
-                    out = new DataOutputStream((socket.getOutputStream()));
-                    new Thread(()->{
-                        try{
-                            while (true){
-                                String strFromServer = in.readUTF();
-                                if (strFromServer.startsWith("/authok")){
-                                    nick = strFromServer.split(" ")[1];
-                                    chatArea.append("# Вы авторизованы как: " +nick + "\n");
-                                    break;
-                                }
-                                chatArea.append(strFromServer + "\n");
-                            }
-                            while (true){
-                                String strFromServer = in.readUTF();
-                                if (strFromServer.equalsIgnoreCase("/end")){
-                                    break;
-                                }
-                                if (strFromServer.startsWith("/clients")){
-                                    String clients = strFromServer.substring(9);
-                                    clientsList.setText(clients.replace(' ','\n'));
-                                }
-                                else {
-                                    chatArea.append(strFromServer + "\n");
-                                }
-                            }
-                        }catch (Exception e){
-                            e.printStackTrace();
-                        }finally {
-                            closeConnection();
-                        }
-                    }).start();
+                if (socket == null || socket.isClosed() && !timeOutStatus){
+                    openConnection();
+                    startTread();
                 }
                 out.writeUTF(trimmedMessage);
             }catch (IOException e){
@@ -101,7 +131,7 @@ public class NetworkChat extends JFrame {
             }
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new NetworkChat();
     }
 }
